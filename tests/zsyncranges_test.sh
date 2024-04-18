4c989bccf006def1505494b55bb73c0ec22eb61c
Laurenz <git@laure.nz>
2024-04-18 22:02:45 +0200

    update tests

--- tests/zsyncranges_test.sh
+++ tests/zsyncranges_test.sh
@@ -11,44 +11,50 @@ fi
 test "$out" == "/invalid: No such file or directory"
 separator
 
-# Need full file
+#----------------------------------------------------------------
+echo Need full file
 ranges="$(./zsyncranges "$(pwd)/tests/loremipsum.zsync" /dev/null)"
-test "$ranges" == '{"ranges":[[0,1063]]}'
+test "$ranges" == '{"reuse":[],"download":[[0,1063]]}'
 separator
 
-# File is complete, need nothing
+#----------------------------------------------------------------
+echo File is complete, need nothing
 ranges="$(./zsyncranges "$(pwd)/tests/loremipsum.zsync" "$(pwd)/tests/files/loremipsum")"
-test "$ranges" == '{"ranges":[]}'
+test "$ranges" == '{"reuse":[[0,0,1064]],"download":[]}'
 separator
 
-# Changed file, need partial update
+#----------------------------------------------------------------
+echo Changed file, need partial update
 cp tests/files/loremipsum "$TEST_TMPDIR/seed"
 sed -i 's/massa/xxxxx/g' "$TEST_TMPDIR/seed"
 # changed locations are at "grep -ob massa tests/files/loremipsum | cut -d: -f1": 172, 451, 566, 803
 ranges="$(./zsyncranges "$(pwd)/tests/loremipsum.zsync" "$TEST_TMPDIR/seed")"
 # Ranges fall at change locations
-test "$ranges" == '{"ranges":[[168,183],[448,455],[560,575],[800,807]]}'
+test "$ranges" == '{"reuse":[[0,0,168]],"download":[[168,183],[448,455],[560,575],[800,807]]}'
 separator
 
-# Removed some blocks from the front, need partial update
+#----------------------------------------------------------------
+echo Removed some blocks from the front, need partial update
 dd if=tests/files/loremipsum of="$TEST_TMPDIR/seed" bs=1 skip=81
 ranges="$(./zsyncranges "$(pwd)/tests/loremipsum.zsync" "$TEST_TMPDIR/seed")"
 # 81B gone, need 11 blocks (blocksize 8B - 88B) replaced
-test "$ranges" == '{"ranges":[[0,87]]}'
+test "$ranges" == '{"reuse":[[88,7,976]],"download":[[0,87]]}'
 separator
 
-# Added some blocks to the front
+#----------------------------------------------------------------
+echo Added some blocks to the front
 dd if=tests/files/loremipsum of="$TEST_TMPDIR/seed" bs=1 skip=81
 cat - tests/files/loremipsum <<<"This is extra data to be removed" >"$TEST_TMPDIR/seed"
 cat "$TEST_TMPDIR/seed"
 ranges="$(./zsyncranges "$(pwd)/tests/loremipsum.zsync" "$TEST_TMPDIR/seed")"
-test "$ranges" == '{"ranges":[]}' # TODO: fix!
+test "$ranges" == '{"reuse":[[0,33,1064]],"download":[]}' # TODO: fix!
 separator
 
-# Added some blocks to the back
+#----------------------------------------------------------------
+echo Added some blocks to the back
 dd if=tests/files/loremipsum of="$TEST_TMPDIR/seed" bs=1 skip=81
 cat tests/files/loremipsum - <<<"This is extra data to be removed" >"$TEST_TMPDIR/seed"
 cat "$TEST_TMPDIR/seed"
 ranges="$(./zsyncranges "$(pwd)/tests/loremipsum.zsync" "$TEST_TMPDIR/seed")"
-test "$ranges" == '{"ranges":[]}' # TODO: fix!
+test "$ranges" == '{"reuse":[[0,0,1064]],"download":[]}' # TODO: fix!
 separator
